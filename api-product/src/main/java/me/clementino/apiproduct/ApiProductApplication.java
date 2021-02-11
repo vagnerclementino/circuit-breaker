5d18fafd8077e4740a5904439312ee12292b8998
Vagner Clementino <vagner.clementino@hotmart.com>
2021-02-10 23:07:13 -0300

    Adds api-prodcut base files

---
+++ api-product/src/main/java/me/clementino/apiproduct/ApiProductApplication.java
@@ -0,0 +1,13 @@
+package me.clementino.apiproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProductApplication.class, args);
+	}
+
+}
